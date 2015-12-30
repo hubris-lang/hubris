84680cfa057a5008daf2a847b8080ccfcf76f245
Jared Roesch <roeschinc@gmail.com>
2015-12-30 04:21:22 -0800

    Add basic spanned error code

--- src/hubris/typeck/error_reporting.rs
+++ src/hubris/typeck/error_reporting.rs
@@ -1,13 +1,27 @@
 use super::{Error, TyCtxt};
+use super::super::ast::Span;
+
+pub fn span_error(ty_cx: &TyCtxt, span: Span, message: String) {
+    let (line_no, col_no) = ty_cx.source_map.position(span).unwrap();
+    let (line, offset) = ty_cx.source_map.find_line(span.lo).unwrap();
+    let format = format!("filename:{}:{}: {}:{} error: {}\n{}",
+        line_no, col_no, line_no, col_no, message, line);
+    panic!("{}", format);
+}
 
 pub fn report_type_error(ty_cx: &TyCtxt, e: Error) {
     match e {
         Error::UnknownVariable(name) => {
-            let (offset, line) = ty_cx.source_map.find_line(name.span.lo).unwrap();
-            panic!("unknown variable `{}` on: `{}`", name, line);
+            span_error(
+                ty_cx,
+                name.span,
+                format!("unknown variable `{}`", name));
         },
-        Error::UnificationErr(_, t1, t2) => {
-            panic!("unification failed `{}` is not equivalent to `{}`", t1, t2);
+        Error::UnificationErr(span, t1, t2) => {
+            span_error(
+                ty_cx,
+                span,
+                format!("unification failed `{}` is not equivalent to `{}`", t1, t2));
         }
         err => panic!("error encountered while type checking: {:?}", err),
     }
