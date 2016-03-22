51c982922b980cd97dc0fc31273a0e6dcbd031b0
Jared Roesch <roeschinc@gmail.com>
2016-03-21 19:19:39 -0700

    Update the repo, so we build on the newest versions

--- src/hubris/server.rs
+++ src/hubris/server.rs
@@ -3,26 +3,23 @@ use iron::status;
 use router::Router;
 use urlencoded::UrlEncodedQuery;
 
-// struct Session {
-//
-// }
+fn handler(req: &mut Request) -> IronResult<Response> {
+    // match req.get_ref::<UrlEncodedQuery>() {
+    //     Err(ref e) => {
+    //         let err_msg = format!("{:?}", e);
+    //         Ok(Response::with((status::Ok, &err_msg[..])))
+    //     },
+    //     Ok(ref hashmap) => {
+    //         println!("Parsed GET request query string:\n {:?}", hashmap);
+    //         Ok(Response::with((status::Ok, "he")))
+    //     }
+    // }
+    panic!("fix me, something in Iron broke")
+}
+
 pub fn run() {
     let mut router = Router::new();  // Alternative syntax:
     router.get("/check", handler);  //                      get "/:query" => handler);
 
     Iron::new(router).http("127.0.0.1:3000").unwrap();
-
-    fn handler(req: &mut Request) -> IronResult<Response> {
-        match req.get_ref::<UrlEncodedQuery>() {
-            Err(ref e) => {
-                let err_msg = format!("{:?}", e);
-                Ok(Response::with((status::Ok, &err_msg[..])))
-            },
-            Ok(ref hashmap) => {
-                println!("Parsed GET request query string:\n {:?}", hashmap);
-                Ok(Response::with((status::Ok, "he")))
-            }
-        }
-
-    }
 }
